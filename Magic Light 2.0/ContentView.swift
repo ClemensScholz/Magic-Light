70a421c4694a5f02f6a7fbc9e1029fa7f4927c18
ClemensScholz <scholz.clemens@gmail.com>
2021-01-17 13:29:54 +0100

    Initial Commit

---
+++ Magic Light 2.0/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Magic Light 2.0
+//
+//  Created by Clemens on 17.01.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Magic Light 2.0/Magic_Light_2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  Magic_Light_2_0App.swift
+//  Magic Light 2.0
+//
+//  Created by Clemens on 17.01.21.
+//
+
+import SwiftUI
+
+@main
+struct Magic_Light_2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
